5a17859713be3051968c98af1c2e975f225f79c3
abhishek-end <abhishekyadav49534@gmail.com>
2024-08-30 23:28:43 +0530

    structure complete

---
+++ client/expense tracker/src/App.jsx
@@ -0,0 +1,8 @@
+// import "./../index.css";
+export default function App() {
+  return (
+    <div className='App'>
+      <h1 className='text-2xl text-red-500'>Expense Tracker</h1>
+    </div>
+  );
+}
